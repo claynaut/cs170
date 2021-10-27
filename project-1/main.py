b2db466de31d05123191b1c7908033919e07b724
claynaut <jpesc002@ucr.edu>
2021-10-27 13:44:48 -0700

    Reorganize functions, add more user interface stuff

--- project-1/main.py
+++ project-1/main.py
@@ -1,11 +1,24 @@
-# creates an 8-puzzle based on the user's option
-#   option - user's option which determines which layout to use
-def makePuzzle(option):
+# creates an 8-puzzle based on the user's option and returns the generated 8-puzzle
+def selectPuzzle():
+    print('Select a puzzle to solve for this 8-puzzle solver.')
+    print('\t1. Use default puzzle layout')
+    print('\t2. Create a custom puzzle layout')
+
+    # retrieves user's selection for which puzzle to make
+    option  = int(input('\nChoose an option: '))
+
+    # if user selects an invalid option, loop until a valid input is made
+    while (option != 1 and option != 2):
+        print('Error: Invalid option. Try again.')
+        option  = int(input('Choose an option: '))
+
     # returns default puzzle layout
     if (option == 1):
         print('\nUsing default puzzle layout...')
+
         # returns default puzzle
         return [[1, 2, 3], [4, 5 , 6], [7, 8, 0]]
+
     # creates a custom puzzle layout
     elif (option == 2):
         print('\nCreating custom puzzle layout...')
@@ -74,49 +87,57 @@ def makePuzzle(option):
 
         # returns user-created puzzle
         return [row1, row2, row3]
-    # if user doesn't select options 1-2, a -1 is returned to indicate an error
-    else:
-        return -1;
 
-# prints menu to select a puzzle in console
-def printPuzzleMenu():
-    print('Select a puzzle to solve for this 8-puzzle solver.')
-    print('\t1. Use default puzzle layout')
-    print('\t2. Create a custom puzzle layout')
-
-# prints menu to select an algorithm in console
-def printAlgorithmMenu():
+# asks user which algorithm to use and returns a number indicating which algorithm to use
+def selectAlgorithm():
     print('\nSelect algorithm.')
     print('\t1. Uniform Cost Search')
-    print('\t2. Misplaced Tile heuristic')
-    print('\t2. Manhattan Distance heuristic')
-
-# main function
-def main():
-    # prints menu for selecting a puzzle
-    printPuzzleMenu()
-    
-    # retrieves user's selection on which puzzle to use
-    option  = int(input('\nChoose an option: '))
-
-    # generate a puzzle based on user's selection
-    puzzle = makePuzzle(option)
-    # if function returns a -1, the user choses an invalid option
-    # thus, continue retrieving user input until a valid option is inputted
-    while (puzzle == -1):
-        print('Error: Invalid option. Try again.')
-        option  = int(input('Choose an option: '))
-        puzzle = makePuzzle(option)
-
-    # print selected puzzle in console
-    for row in puzzle: 
-        print(row)
-        
-    # print menu for selecting an algorithm
-    printAlgorithmMenu()
+    print('\t2. A* with Misplaced Tile heuristic')
+    print('\t2. A* Manhattan Distance heuristic')
 
     # retrieves user's selection on which algorithm to use
     option  = int(input('\nChoose an option: '))
+
+    # if user selects an invalid option, loop until a valid input is made
+    while (option != 1 and option != 2 and option != 3):
+        print('Error: Invalid option. Try again.')
+        option  = int(input('Choose an option: '))
+
+    # returns user's selection
+    return option
+
+# prints out which algorithm will be used and calls the respective function for that algorithm
+def runAlgorithm(option):
+    # runs algorithm for uniform cost search
+    if (option == 1):
+        print('\nSolving with Uniform Cost Search...')
+
+    # runs algorithm for A* with misplaced tile heuristic
+    elif (option == 2):
+        print('\Solving with A* with Misplaced Tile heuristic...')
+
+    # runs algorithm for A* with manhattan distance heuristic
+    elif (option == 3):
+        print('\Solving with A* Manhattan Distance heuristic...')
+
+# prints puzzle in console
+def printPuzzle(puzzle):
+    for row in puzzle: 
+        print(row)
+
+# main function
+def main():
+    # asks user which puzzle to make and makes the desired puzzle
+    puzzle = selectPuzzle()
+
+    # prints selected puzzle in console
+    printPuzzle(puzzle)
+        
+    # prints menu for selecting an algorithm
+    algorithm = selectAlgorithm()
+
+    # starts running selected algorithm
+    runAlgorithm(algorithm)
 
 # runs main function
 main()
