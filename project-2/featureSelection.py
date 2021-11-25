66bb7c0e74f624bde3a29b5247fb03671d4af2e6
claynaut <jpesc002@ucr.edu>
2021-11-25 15:03:21 -0800

    Implement knn algorithm, calculate accuracy

--- project-2/featureSelection.py
+++ project-2/featureSelection.py
@@ -1,10 +1,60 @@
 from anytree import Node # nodes used to create objects with custom attributes
-import copy # deep copy used to make a copy of a value to avoid altering a reference
+import copy as cp # deep copy used to make a copy of a value to avoid altering a reference
+import numpy as np
+
+# calculate accuracy of classification by k nearest neighbor
+def accuracy(data):
+    accuracy = 0
+    for node in data:
+        if node.classification == node.knnclassification:
+            accuracy = accuracy + 1
+    return (accuracy / len(data))
+
+# performs a k nearest neighbor search algorithm
+# note that faeturse are offset by -1 due to the nature of indices in an array
+def knnSearch(data, features, k):
+    nodes = cp.deepcopy(data)
+    for x in range(len(nodes)):
+        # make an array of features to compare from test node
+        a = np.array([nodes[x].features[i] for i in features])
+
+        # make a list of neighbors excluding the test node
+        neighbors = cp.deepcopy(data)
+        del neighbors[x]
+
+        # calculate the distance from test node to every other node
+        for y in range(len(neighbors)):
+            b = np.array([neighbors[y].features[i] for i in features])
+            dist = np.linalg.norm(a-b) # uses built-in Euclidean distance algorithm
+            neighbors[y].dist = dist
+
+        # sort by smallest distances and slice array to get k neighbors
+        neighbors.sort(key = lambda node: node.dist)
+        neighbors = neighbors[:k]
+
+        # count how many of each class is present in list of neighbors
+        class1_cnt = 0
+        class2_cnt = 0
+        for n in neighbors:
+            if n.classification == 1:
+                class1_cnt = class1_cnt + 1
+            else:
+                class2_cnt = class2_cnt + 1
+        
+        # use majority class count to determine class of current test node
+        if class1_cnt > class2_cnt:
+            nodes[x].knnclassification = 1
+        else:
+            nodes[x].knnclassification = 2
+
+    # return list of nodes with a new attribute of a classification determined by algorithm
+    return nodes
 
 # reads the dataset based on filename and returns a list of nodes with corresponding data
 def getNodes(filename):
     cnt = 0 # used to name nodes
     nodes = []
+
     # open corresponding file chosen by the user
     with open(filename) as f:
         for line in f: # read each line and perform the following actions
@@ -22,6 +72,7 @@ def getNodes(filename):
             node.features = features # give the node all of its features
             nodes.append(node) # add node to a list of nodes to return
             cnt = cnt + 1
+
     # returns a list of nodes created using the dataset
     return nodes
 
@@ -31,5 +82,5 @@ def main():
     filename = input('Enter a filename to test: ')
     nodes = getNodes(filename)
     print('\nThis dataset has', len(nodes[0].features), 'features with', len(nodes), 'instances.')
-
+    
 main()
